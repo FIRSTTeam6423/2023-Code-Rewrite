6eb2423a0dd6b66fcf63aa6d842ad45e695b41af
dabeycorn <dasun20202020@gmail.com>
2024-01-03 19:04:49 -0800

    general fixes

--- src/main/java/frc/robot/subsystems/VisionUtil.java
+++ src/main/java/frc/robot/subsystems/VisionUtil.java
@@ -4,68 +4,12 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class VisionUtil extends SubsystemBase {
-  /** Creates a new VisionUtil. */
-  public VisionUtil() {}
+public class e extends SubsystemBase {
+  /** Creates a new e. */
+  public e() {}
 
-  public static Pose3d getTagPose3dFromId(int id) {
-		return Constants.TagPoses[id - 1];
-	}
-
-	public static PhotonTrackedTarget getNearestCameraTarget() {
-		PhotonPipelineResult result = camera.getLatestResult();
-		if (result.hasTargets()) {
-			return result.getBestTarget();
-		}
-		return null;
-	}
-
-	public static List<PhotonTrackedTarget> getAllCameraTargets() {
-		PhotonPipelineResult result = camera.getLatestResult();
-		if (result.hasTargets()) {
-			return result.getTargets();
-		} else {
-			return Collections.<PhotonTrackedTarget>emptyList();
-		}
-	}
-
-	public static Pose3d getPose3dOfNearestCameraTarget() {
-		PhotonPipelineResult result = camera.getLatestResult();
-		if (result.hasTargets()) {
-			PhotonTrackedTarget target = result.getBestTarget();
-			Pose3d tagPose = getTagPose3dFromId(target.getFiducialId());
-			return tagPose;
-		}
-    DriverStation.reportWarning("No nearest camera target to get Pose3d!", false);
-		return null;
-	}
-
-	public static Pose2d getFieldPosed2dFromNearestCameraTarget() {
-		PhotonPipelineResult result = camera.getLatestResult();
-		if (result.hasTargets()) {
-			PhotonTrackedTarget target = result.getBestTarget();
-			Pose3d tagPose = getTagPose3dFromId(target.getFiducialId());
-			Pose3d pos = PhotonUtils.estimateFieldToRobotAprilTag(
-          target.getBestCameraToTarget(),   
-          tagPose,
-          Constants.CAMERA_TO_ROBOT // TODO: ADD THIS
-					);
-      allianceOrientation = Math.toDegrees(tagPose.getRotation().getZ());
-			return new Pose2d(
-					pos.getX(),
-					pos.getY(),
-					new Rotation2d(pos.getRotation().getZ()));
-		}
-		DriverStation.reportWarning("Could not get Pose2d from camera target: no targets found.", false);
-		return null;
-  }
-  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
