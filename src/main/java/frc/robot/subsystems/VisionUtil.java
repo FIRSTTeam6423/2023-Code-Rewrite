fa53d110373a81c231640a218c9b357037e185ca
dabeycorn <dasun20202020@gmail.com>
2024-01-03 18:52:25 -0800

    Moved all Vision methods from RobotContainer to a new Vision subsystem

---
+++ src/main/java/frc/robot/subsystems/VisionUtil.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionUtil extends SubsystemBase {
+  /** Creates a new VisionUtil. */
+  public VisionUtil() {}
+
+  public static Pose3d getTagPose3dFromId(int id) {
+		return Constants.TagPoses[id - 1];
+	}
+
+	public static PhotonTrackedTarget getNearestCameraTarget() {
+		PhotonPipelineResult result = camera.getLatestResult();
+		if (result.hasTargets()) {
+			return result.getBestTarget();
+		}
+		return null;
+	}
+
+	public static List<PhotonTrackedTarget> getAllCameraTargets() {
+		PhotonPipelineResult result = camera.getLatestResult();
+		if (result.hasTargets()) {
+			return result.getTargets();
+		} else {
+			return Collections.<PhotonTrackedTarget>emptyList();
+		}
+	}
+
+	public static Pose3d getPose3dOfNearestCameraTarget() {
+		PhotonPipelineResult result = camera.getLatestResult();
+		if (result.hasTargets()) {
+			PhotonTrackedTarget target = result.getBestTarget();
+			Pose3d tagPose = getTagPose3dFromId(target.getFiducialId());
+			return tagPose;
+		}
+    DriverStation.reportWarning("No nearest camera target to get Pose3d!", false);
+		return null;
+	}
+
+	public static Pose2d getFieldPosed2dFromNearestCameraTarget() {
+		PhotonPipelineResult result = camera.getLatestResult();
+		if (result.hasTargets()) {
+			PhotonTrackedTarget target = result.getBestTarget();
+			Pose3d tagPose = getTagPose3dFromId(target.getFiducialId());
+			Pose3d pos = PhotonUtils.estimateFieldToRobotAprilTag(
+          target.getBestCameraToTarget(),   
+          tagPose,
+          Constants.CAMERA_TO_ROBOT // TODO: ADD THIS
+					);
+      allianceOrientation = Math.toDegrees(tagPose.getRotation().getZ());
+			return new Pose2d(
+					pos.getX(),
+					pos.getY(),
+					new Rotation2d(pos.getRotation().getZ()));
+		}
+		DriverStation.reportWarning("Could not get Pose2d from camera target: no targets found.", false);
+		return null;
+  }
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
